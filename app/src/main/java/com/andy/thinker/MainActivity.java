aa2af092d386262c7584d4d8b456cd4f7fd4ef3c
jianwei <jianwei.chen@microvirt.com>
2019-05-22 17:57:19 +0800

    Complete code part
    Signed-off-by: jianwei <jianwei.chen@microvirt.com>

--- app/src/main/java/com/andy/thinker/MainActivity.java
+++ app/src/main/java/com/andy/thinker/MainActivity.java
@@ -1,29 +1,138 @@
 package com.andy.thinker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+import com.tencent.tinker.loader.shareutil.ShareTinkerInternals;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mLoadPath;
+    private Button mClearPath;
+    private Button mToast;
+    private Button mShowInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        initView();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void initView() {
+        mLoadPath = (Button) findViewById(R.id.btn_load);
+        mClearPath = (Button) findViewById(R.id.btn_clear);
+        mToast = (Button) findViewById(R.id.btn_toast);
+        mShowInfo = (Button) findViewById(R.id.btn_show);
+
+        mLoadPath.setOnClickListener(this);
+        mClearPath.setOnClickListener(this);
+        mToast.setOnClickListener(this);
+        mShowInfo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_load:
+                loadPath();
+                break;
+
+            case R.id.btn_clear:
+                clearPath();
+                break;
+
+            case R.id.btn_toast:
+                Toast.makeText(getApplicationContext(),"使用Tinker修改后的内容",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btn_show:
+                showInfo(this);
+                break;
+        }
+    }
+
+    //加载补丁
+    private void loadPath() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/patch_signed_7zip.apk";
+        File file = new File(path);
+        if (file.exists()){
+            Toast.makeText(this, "补丁存在", Toast.LENGTH_SHORT).show();
+            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), path);
+        }else {
+            Toast.makeText(this, "补丁不存在", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //清除补丁
+    private void clearPath() {
+        Tinker.with(getApplicationContext()).cleanPatch();
+
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath()+"/patch_signed_7zip.apk";
+        File file = new File(path);
+        if (file.exists()){
+            Toast.makeText(this, "补丁存在", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "补丁不存在", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public boolean showInfo(Context context) {
+        // add more Build Info
+        final StringBuilder sb = new StringBuilder();
+        Tinker tinker = Tinker.with(getApplicationContext());
+        if (tinker.isTinkerLoaded()) {
+            sb.append(String.format("[patch is loaded] \n"));
+            sb.append(String.format("[buildConfig TINKER_ID] %s \n", "1.7.7"));
+            sb.append(String.format("[buildConfig BASE_TINKER_ID] %s \n", "1.7.7"));
+
+            sb.append(String.format("[buildConfig MESSSAGE] %s \n", "I am the base apk"));
+            sb.append(String.format("[TINKER_ID] %s \n", tinker.getTinkerLoadResultIfPresent().getPackageConfigByName(ShareConstants.TINKER_ID)));
+            sb.append(String.format("[packageConfig patchMessage] %s \n", tinker.getTinkerLoadResultIfPresent().getPackageConfigByName("patchMessage")));
+            sb.append(String.format("[TINKER_ID Rom Space] %d k \n", tinker.getTinkerRomSpace()));
+
+        } else {
+            sb.append(String.format("[patch is not loaded] \n"));
+            sb.append(String.format("[buildConfig TINKER_ID] %s \n", "1.9.1"));
+            sb.append(String.format("[buildConfig BASE_TINKER_ID] %s \n","1.9.1"));
+
+            sb.append(String.format("[buildConfig MESSSAGE] %s \n", "I am the base apk"));
+            sb.append(String.format("[TINKER_ID] %s \n", ShareTinkerInternals.getManifestTinkerID(getApplicationContext())));
+        }
+        sb.append(String.format("[BaseBuildInfo Message] %s \n", ""));
+
+        final TextView v = new TextView(context);
+        v.setText(sb);
+        v.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+        v.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10);
+        v.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        v.setTextColor(0xFF000000);
+        v.setTypeface(Typeface.MONOSPACE);
+        final int padding = 16;
+        v.setPadding(padding, padding, padding, padding);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(true);
+        builder.setView(v);
+        final AlertDialog alert = builder.create();
+        alert.show();
+        return true;
+    }
 }
---
+++ app/src/main/java/com/andy/thinker/SimpleTinkerApplicationLike.java
@@ -0,0 +1,43 @@
+package com.andy.thinker;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.multidex.MultiDex;
+import android.util.Log;
+
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+@DefaultLifeCycle(application = ".TinkerInApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class SimpleTinkerApplicationLike extends DefaultApplicationLike {
+
+    private static final String TAG = "TinkerApplicationLike";
+
+    public SimpleTinkerApplicationLike(Application application, int tinkerFlags,
+                                       boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime,
+                                       long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime,
+                applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        MultiDex.install(base);
+        //初始化Tinker
+        TinkerInstaller.install(this);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+        Log.d(TAG, "SimpleTinkerInApplicationLike的registerActivityLifecycleCallbacks方法执行了");
+    }
+}
